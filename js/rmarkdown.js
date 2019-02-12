07991577cd9191d26c8da383bcaa1653314898e6
Alx <erasmus-rudn@yandex.ru>
2019-02-12 21:43:57 +0300

    New quiz based on dplyr and rnoaa meteo data

--- js/rmarkdown.js
+++ js/rmarkdown.js
@@ -38,6 +38,44 @@ var RMD = {};
 }());
 
 $(document).ready(function() {
+
+  $chunks = $('.fold');
+
+  $chunks.each(function () {
+
+    // add button to source code chunks
+    if ( $(this).hasClass('s') ) {
+      $('pre.r', this).prepend("<div class=\"showopt\">Открыть исходный код</div><br style=\"line-height:22px;\"/>");
+      $('pre.r', this).children('code').attr('class', 'folded');
+    }
+
+    // add button to output chunks
+    if ( $(this).hasClass('o') ) {
+      $('pre:not(.r)', this).has('code').prepend("<div class=\"showopt\">Открыть результат</div><br style=\"line-height:22px;\"/>");
+      $('pre:not(.r)', this).children('code:not(r)').addClass('folded');
+
+      // add button to plots
+      $(this).find('img').wrap('<pre class=\"plot\"></pre>');
+      $('pre.plot', this).prepend("<div class=\"showopt\">Открыть график</div><br style=\"line-height:22px;\"/>");
+      $('pre.plot', this).children('img').addClass('folded');
+
+    }
+  });
+
+  // hide all chunks when document is loaded
+  $('.folded').css('display', 'none')
+
+  // function to toggle the visibility
+  $('.showopt').click(function() {
+    var label = $(this).html();
+    if (label.indexOf("Show") >= 0) {
+      $(this).html(label.replace("Открыть ", "Скрыть "));
+    } else {
+      $(this).html(label.replace("Скрыть ", "Открыть "));
+    }
+    $(this).siblings('code, img').slideToggle('fast', 'swing');
+  });
+
   RMD.app = new RMD.AppViewModel();
   RMD.app.init();
 });
